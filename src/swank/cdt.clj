39cfc42c363d5bc04a61e49e7c1aebbc4a026f9e
George Jahad <george@georgejahad.com>
2011-03-26 19:52:28 -0700

    removed cutils

--- src/swank/cdt.clj
+++ src/swank/cdt.clj
@@ -2,7 +2,6 @@
   ;; convenience namespace to give users easy access to main functions
   (:refer-clojure :exclude [next])
   (:require [cdt.ui :as cdt]
-            [swank.core.cdt-utils :as cutils]
             [swank.core.cdt-backends :as cbackends]))
 
 (cdt/expose cbackends/set-catch cbackends/set-bp cbackends/reval
