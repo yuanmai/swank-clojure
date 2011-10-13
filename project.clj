3ab4ea3075a60120ec5dbc638e07b794c480676c
Phil Hagelberg <technomancy@gmail.com>
2011-10-13 14:52:49 -0700

    Bump version number to 1.3.4-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swank-clojure "1.3.3"
+(defproject swank-clojure "1.3.4-SNAPSHOT"
   :description "Swank server connecting Clojure to Emacs SLIME"
   :url "http://github.com/technomancy/swank-clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]]
