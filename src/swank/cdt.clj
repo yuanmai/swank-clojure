036a9be4ded25ea3e3022cedd54c278519202419
Tavis Rudd <tavis@damnsimple.com>
2011-12-27 14:20:25 -0800

    inc version number

--- src/swank/cdt.clj
+++ src/swank/cdt.clj
@@ -4,7 +4,7 @@
   (:require [cdt.ui :as cdt]
             [swank.core.cdt-backends :as cbackends]))
 
-(def swank-cdt-release "1.4.0a")
+(def swank-cdt-release "1.5.0a")
 
 (cdt/expose cbackends/set-catch cbackends/set-bp cbackends/reval
             cdt/delete-catch cdt/delete-bp cdt/delete-all-breakpoints
