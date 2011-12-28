3d5b0ff782440eb6cee8508a543ed8ed7f1af0bd
Tavis Rudd <tavis@damnsimple.com>
2011-12-28 14:15:30 -0800

    add tools.jar and sa-jdi.jar to :extra-classpath-dirs if found
    This prevents CDT bootstrap failure.

--- src/leiningen/swank.clj
+++ src/leiningen/swank.clj
@@ -20,9 +20,21 @@
                 [:host host :port (Integer. port)
                  :repl-out-root true :block true]))))
 
+(defn- add-jdk-toolsjar-to-classpath
+  "CDT requires the JDK's tools.jar and sa-jdi.jar. Add them to the classpath."
+  [project]
+  (let [libdir (format "%s/../lib" (System/getProperty "java.home"))
+        f-exists? (fn [path] (.exists (File. path)))
+        extra-cp (filter f-exists? (map #(str libdir "/" %)
+                                        ["tools.jar" "sa-jdi.jar"]))
+        cp-key :extra-classpath-dirs]
+    (assoc project cp-key (apply conj (cp-key project []) extra-cp))))
+
 (defn swank
   "Launch swank server for Emacs to connect. Optionally takes PORT and HOST."
   ([project port host & opts]
-     (eval-in-project project (swank-form project port host opts)))
+     (eval-in-project
+      (add-jdk-toolsjar-to-classpath project)
+      (swank-form project port host opts)))
   ([project port] (swank project port "localhost"))
   ([project] (swank project 4005)))
