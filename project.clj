09f481b1023816f0985a2e4b3a43b557910a2807
Tavis Rudd <tavis@damnsimple.com>
2011-12-28 15:03:14 -0800

    merge upstream changes for 1.3.4 release (mostly docs)

--- project.clj
+++ project.clj
@@ -2,8 +2,9 @@
   :description "Swank server connecting Clojure to Emacs SLIME"
   :url "http://github.com/technomancy/swank-clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [clj-stacktrace "0.2.4-SNAPSHOT"]
+                 [clj-stacktrace "0.2.4"]
                  [cdt "1.2.6.2-SNAPSHOT"]]
   :dev-dependencies [[lein-multi "1.0.0"]]
-  :multi-deps {"1.3" [[org.clojure/clojure "1.3.0"]]}
+  :multi-deps {"1.3" [[org.clojure/clojure "1.3.0"]
+                      [clj-stacktrace "0.2.4"]]}
   :shell-wrapper {:main swank.swank})
