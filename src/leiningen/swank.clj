a8f47f3cf04ac25c137c088582cced88574582e8
Tavis Rudd <tavis@damnsimple.com>
2012-01-02 18:45:05 -0800

    make sure add-cdt-project-args doesn't load jvm TI agent twice in :jvm-opts

--- src/leiningen/swank.clj
+++ src/leiningen/swank.clj
@@ -23,6 +23,12 @@
 (def ^{:private true} jvm-opts
   "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n")
 
+(defn- add-cdt-jvm-opts [project]
+  (if (seq (filter #(re-find #"jdwp" %)
+                   (:jvm-opts project)))
+    project
+    (update-in project [:jvm-opts] conj jvm-opts)))
+
 (defn- add-cdt-project-args
   "CDT requires the JDK's tools.jar and sa-jdi.jar. Add them to the classpath."
   [project]
@@ -33,7 +39,7 @@
                      (.getAbsolutePath (io/file libdir j)))]
       (-> project
           (update-in [:extra-classpath-dirs] concat extra-cp)
-          (update-in [:jvm-opts] conj jvm-opts)))
+          add-cdt-jvm-opts))
     project))
 
 (defn swank
