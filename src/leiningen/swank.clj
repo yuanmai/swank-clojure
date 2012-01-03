a0e6ab02cedeaab26c6f9efc22be25f12d7ed65a
Phil Hagelberg <technomancy@gmail.com>
2012-01-02 16:45:29 -0800

    Add :jvm-opts and fix tools.jar paths to work with Debian.

--- src/leiningen/swank.clj
+++ src/leiningen/swank.clj
@@ -20,20 +20,24 @@
                 [:host host :port (Integer. port)
                  :repl-out-root true :block true]))))
 
-(defn- add-jdk-toolsjar-to-classpath
+(def ^{:private true} jvm-opts
+  "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n")
+
+(defn- add-cdt-project-args
   "CDT requires the JDK's tools.jar and sa-jdi.jar. Add them to the classpath."
   [project]
-  (let [libdir (io/file (System/getProperty "java.home") "lib")
+  (let [libdir (io/file (System/getProperty "java.home") ".." "lib")
         extra-cp (for [j ["tools.jar" "sa-jdi.jar"]
                        :when (.exists (io/file libdir j))]
                    (.getAbsolutePath (io/file libdir j)))]
-    (update-in project [:extra-classpath-dirs] concat extra-cp)))
+    (-> project
+        (update-in [:extra-classpath-dirs] concat extra-cp)
+        (update-in [:jvm-opts] conj jvm-opts))))
 
 (defn swank
   "Launch swank server for Emacs to connect. Optionally takes PORT and HOST."
   ([project port host & opts]
-     (eval-in-project
-      (add-jdk-toolsjar-to-classpath project)
-      (swank-form project port host opts)))
+     (eval-in-project (add-cdt-project-args project)
+                      (swank-form project port host opts)))
   ([project port] (swank project port "localhost"))
   ([project] (swank project 4005)))
