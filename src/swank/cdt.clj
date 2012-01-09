dc7bdf42fa889c8fe350fef3d0fe6de06dfbe818
Tavis Rudd <tavis@damnsimple.com>
2012-01-08 19:02:42 -0800

    added version of cdt's print-bps that prints at right place in repl

--- src/swank/cdt.clj
+++ src/swank/cdt.clj
@@ -2,12 +2,22 @@
   ;; convenience namespace to give users easy access to main functions
   (:refer-clojure :exclude [next])
   (:require [cdt.ui :as cdt]
+            [cdt.events]
+            [swank.core]
             [swank.core.cdt-backends :as cbackends]))
 
 (def swank-cdt-release "1.5.0a")
 
 (cdt/expose cbackends/set-catch cbackends/set-bp cbackends/reval
             cdt/delete-catch cdt/delete-bp cdt/delete-all-breakpoints
-            cdt/print-bps cdt/bg)
+            cdt/bg)
+
+(defn print-bps []
+  (swank.core/send-to-emacs
+   `(:write-string
+     ~(apply str "\nCDT Breakpoints:\n"
+             (for [[n k] (keep-indexed vector (keys @cdt.events/bp-list))]
+               (format "%s: %s\n" n k)))
+     :repl-result)))
 
 (cbackends/cdt-backend-init swank-cdt-release)
