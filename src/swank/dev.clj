e3e7547fb411d11519ad79b75269d33113737740
Tavis Rudd <tavis@damnsimple.com>
2012-01-22 22:11:24 -0800

    note about macro that can probably be removed

--- src/swank/dev.clj
+++ src/swank/dev.clj
@@ -1,6 +1,8 @@
 (ns swank.dev
   (:use (swank util)))
 
+;;; TODO determine if this is used anywhere in 3rd party code.
+;;; Swank-clojure does NOT use it.
 (defmacro with-swank-io [& body]
   `(binding [*out* @(:writer-redir (first @swank.core.server/connections))]
      ~@body))
