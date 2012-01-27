d229707bc35fe5fb947efc30137904f9779190d9
Tavis Rudd <tavis@damnsimple.com>
2012-01-26 18:29:08 -0800

    rely on cdt 1.2.6.2 final

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://github.com/technomancy/swank-clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [clj-stacktrace "0.2.4"]
-                 [cdt "1.2.6.2-SNAPSHOT"]]
+                 [cdt "1.2.6.2"]]
   :dev-dependencies [[lein-multi "1.0.0"]]
   :multi-deps {"1.3" [[org.clojure/clojure "1.3.0"]
                       [clj-stacktrace "0.2.4"]
-                      [cdt "1.2.6.2-SNAPSHOT"]]}
+                      [cdt "1.2.6.2"]]}
   :warn-on-reflection true
   :shell-wrapper {:main swank.swank})
