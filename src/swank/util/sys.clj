3cd73e65e1d4c3a5ad5fa11ba9dfd2d1b0e006bd
Tavis Rudd <tavis@damnsimple.com>
2012-02-15 20:15:59 -0800

    wrap long line

--- src/swank/util/sys.clj
+++ src/swank/util/sys.clj
@@ -6,7 +6,8 @@
    not be accurate depending on the JVM in which clojure is running
    off of."
   ([]
-     (or (first (.. java.lang.management.ManagementFactory (getRuntimeMXBean) (getName) (split "@")))
+     (or (first (.. java.lang.management.ManagementFactory
+                    (getRuntimeMXBean) (getName) (split "@")))
          (System/getProperty "pid")))
   {:tag String})
 
