7c46f64d487588b28435b68922c6287e9a9bfde9
Phil Hagelberg <technomancy@gmail.com>
2012-03-23 20:43:44 -0700

    Eliminate reflection.
    Conflicts:
    
    	src/swank/util/sys.clj

--- src/swank/util/sys.clj
+++ src/swank/util/sys.clj
@@ -9,5 +9,27 @@
          (System/getProperty "pid")))
   {:tag String})
 
+(defn #^java.lang.Process cmd [p]
+  (.. Runtime getRuntime (exec (str p))))
+
+(defn cmdout [^Process o]
+  (let [r (BufferedReader.
+           (InputStreamReader. (.getInputStream o)))]
+    (line-seq r)))
+
+;; would prefer (= (System/getenv "OSTYPE") "cygwin")
+;; but clojure's java not in cygwin env
+(defn is-cygwin? []
+  (not= nil (try (cmdout (cmd "cygpath c:\\")) (catch Exception e))))
+
+(defn universal-path [path]
+  (if (is-cygwin?)
+    (first (cmdout (cmd (str "cygpath " path))))
+    path))
+
+(defn preferred-user-home-path []
+  (or (System/getenv "HOME")
+      (System/getProperty "user.home")))
+
 (defn user-home-path []
   (System/getProperty "user.home"))
